81673643b91aad7d5b19ffb4f8e9fc51b7384e78
Piaowei <msptonmoy@gmail.com>
2023-10-14 15:04:32 +0600

    chor(utility): utility functions added

---
+++ src/shared/sendResponse.ts
@@ -0,0 +1,49 @@
+import { Response } from 'express';
+
+type IApiReponse<T> = {
+  success: boolean;
+  statusCode: number;
+  message?: string | null;
+  meta?: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+  data?: T | null;
+};
+type ILoginApiReponse<T> = {
+  success: boolean;
+  statusCode: number;
+  message?: string | null;
+  meta?: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+  token?: T | null;
+};
+
+const sendResponse = <T>(res: Response, data: IApiReponse<T>): void => {
+  const responseData: IApiReponse<T> = {
+    success: data.success,
+    statusCode: data.statusCode,
+    message: data.message || null,
+    meta: data.meta || null || undefined,
+    data: data.data || null || undefined,
+  };
+
+  res.status(data.statusCode).json(responseData);
+};
+export const sendloginResponse = <T>(res: Response, data: ILoginApiReponse<T>): void => {
+  const responseData: ILoginApiReponse<T> = {
+    success: data.success,
+    statusCode: data.statusCode,
+    message: data.message || null,
+    meta: data.meta || null || undefined,
+    token: data.token || null || undefined,
+  };
+
+  res.status(data.statusCode).json(responseData);
+};
+
+export default sendResponse;
