51eb79ac04da0532b7483f6c2b0add73266f4ee8
Piaowei <msptonmoy@gmail.com>
2023-10-14 15:03:30 +0600

    chor(interface): common interface

---
+++ src/interfaces/common.ts
@@ -0,0 +1,24 @@
+import { IGenericErrorMessage } from './error';
+
+export type IGenericResponse<T> = {
+  meta: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+  data: T;
+};
+
+export type IGenericErrorResponse = {
+  statusCode: number;
+  message: string;
+  errorMessages: IGenericErrorMessage[];
+};
+
+export type IUser = {
+  userId: string,
+  email: string;
+  role: string,
+  iat: number,
+  exp: number;
+};
